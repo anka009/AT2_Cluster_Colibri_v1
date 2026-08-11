4a1145aacf2f1bf280d1628e61ac628b7032786d
anka009 <Andreas.Kamyschnikow@uks.eu>
2026-08-11 21:37:44 +0200

    Remove code block formatting from comments

--- AT2_Cluster_Colibri_v1.py
+++ AT2_Cluster_Colibri_v1.py
@@ -1,4 +1,4 @@
-```python
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -791,4 +791,4 @@ st.info(
        Serie festlegen und nicht mehr pro Maus verändern.
     """
 )
-```
+
